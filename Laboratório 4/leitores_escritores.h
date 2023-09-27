9a64902d193011d69d5411e56821007803815847
Daniel Machado <danielmc@dcc.ufrj.br>
2023-09-27 16:36:02 -0300

    Laboratório 4 - Implementação do padrão leitores/escritores.

---
+++ Laboratório 4/leitores_escritores.h
@@ -0,0 +1,13 @@
+//implementação da biblioteca a ser utilizada pelo programa concorrente base do laboratório;
+
+void entra_leitura();
+
+void sai_leitura();
+
+void entra_escrita();
+
+void sai_escrita();
+
+void init_vars();
+
+void destroy_vars();
